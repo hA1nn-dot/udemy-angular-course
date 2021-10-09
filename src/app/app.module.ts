3b77809fd9f7acd603b0e701da94933736e17943
Haim Guel Quiroz <hgq_2000@hotmail.com>
2021-10-08 22:11:04 -0500

    Add warningAlert and SuccessAlert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { warningAlertComponent } from './warningAlert/warningAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent,
+    warningAlertComponent
   ],
   imports: [
     BrowserModule
